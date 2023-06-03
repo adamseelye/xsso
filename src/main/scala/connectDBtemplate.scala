f113474178f677073c83f36007c54849b9639630
adamseelye <aseelye@protonmail.com>
2023-06-02 20:20:15 -0600

    added connectDB template

---
+++ src/main/scala/connectDBtemplate.scala
@@ -0,0 +1,6 @@
+// Duplicate this file and insert the correct information
+class connectDBtemplate {
+  val url = "jdbc:mysql://<ip address>:<port>/<schema>"
+  val user = "<username>"
+  val password = "<password>"
+}
